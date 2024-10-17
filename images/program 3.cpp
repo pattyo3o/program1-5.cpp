5106568324d45b411deb42f9c8788709fd1b6d38
pattyo3o <airishpaderanga11@gmail.com>
2024-10-17 15:49:53 +0800

    Program

---
+++ images/program 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1, number2;
+    cout<<"Please enter your first number: ";
+    cin>>number1;
+    cout<<"Please enter your second number: ";
+    cin>>number2;
+    cout<<"Product of the two number is "<<number1 * number2;
+}
---
+++ images/program 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float number1, number2;
+    cout<<"Quantity of apple: ";
+    cin>>number1;
+    cout<<"Price per kilogram of apple: ";
+    cin>>number2;
+    cout<<"The price of apple is "<<number1 * number2;
+}
